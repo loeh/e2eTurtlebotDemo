f0b268d610e65b764a8f56cbb48b986d7d247c56
loeh <itobi14@gmail.com>
2016-09-21 13:53:09 +0200

    added new event string to the event listener

--- event_listener/src/event_listener.py
+++ event_listener/src/event_listener.py
@@ -7,13 +7,21 @@ import requests
 # import std messages 
 from std_msgs.msg import String
 
+baseUrl = 'http://acf7984ae549511e69b640206bb85836-1252824845.eu-central-1.elb.amazonaws.com:5000/orchestrate/api/v1.0/'
+
 def callback(data):
     rospy.loginfo("data: %s", data.data)
     message = data.data.split('/')
     if message[0] == 'closeToBase' and message[1] == 'success':
         rospy.loginfo("successfully reached goal!")
-        r = requests.post('http://acf7984ae549511e69b640206bb85836-1252824845.eu-central-1.elb.amazonaws.com:5000/orchestrate/api/v1.0/dock')
- 
+        r = requests.post(baseUrl + 'dock')
+    elif message[0] == 'docked' and message[1] == 'success':
+        rospy.loginfo("successfully reached docked!")
+        r = requests.post(baseUrl + 'docked')
+    else:
+        rospy.loginfo("not a known event string!")
+
+
 def listener():
 
     rospy.init_node('listener', anonymous=True)
