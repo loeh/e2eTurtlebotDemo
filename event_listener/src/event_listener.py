dc00f136d50abddd6d4d211531eb51eecd272a62
loeh <itobi14@gmail.com>
2016-09-21 09:32:18 +0200

    changed the moveto script to use a normal rostopic event, to signal when it reached it's destination added an event_listener to capture these events and react acordingly

---
+++ event_listener/src/event_listener.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import roslib
+import rospy
+import requests
+
+# import std messages 
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo("data: %s", data.data)
+    message = data.data.split('/')
+    if message[0] == 'closeToBase' and message[1] == 'success':
+        rospy.loginfo("successfully reached goal!")
+        r = requests.post('http://acf7984ae549511e69b640206bb85836-1252824845.eu-central-1.elb.amazonaws.com:5000/orchestrate/api/v1.0/dock')
+ 
+def listener():
+
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber("event", String, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
