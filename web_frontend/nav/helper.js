3b0af86d4aead569959600654f62eff1fb21fb67
loeh <itobi14@gmail.com>
2016-09-27 17:27:28 +0200

    updated the frontend to get the url of the rosbridge via the webapp

--- web_frontend/nav/helper.js
+++ web_frontend/nav/helper.js
@@ -20,3 +20,32 @@ function redirect() {
     setTimeout("window.location.href = 'map.html';", 2000);
   }
 }
+
+function init() {
+  $.get(baseURL + 'getServiceEndPoint', function(data, status){
+      url = data + ':9090';
+      initRos(url);
+  });
+}
+
+function initRos(url) {
+    // Connect to ROS.
+    var ros = new ROSLIB.Ros({
+      url : 'ws://' + url
+    });
+
+    // Create the main viewer.
+    var viewer = new ROS2D.Viewer({
+      divID : 'nav',
+      width : 900,
+      height : 900
+    });
+
+    // Setup the nav client.
+    var nav = NAV2D.OccupancyGridClientNav({
+      ros : ros,
+      rootObject : viewer.scene,
+      viewer : viewer,
+      serverName : '/move_base'
+    });
+}
